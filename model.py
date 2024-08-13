bded90ef4a15b35a61eea4ca8cea60682753c263
kevinleee21 <lee.kevin1@northeastern.edu>
2024-08-13 16:45:31 -0400

    implementation complete, training in progress

--- model.py
+++ model.py
@@ -10,12 +10,6 @@ import torch.nn.functional as F
 import math
 import matplotlib.pyplot as plt
 
-env = knights_archers_zombies_v10.env(render_mode="human", spawn_rate=20, num_archers=2, num_knights=2, max_zombies=10,
-                                      max_arrows=10, killable_knights=True, killable_archers=True, pad_observation=True,
-                                      line_death=False, max_cycles=900,
-                                      vector_state=True, use_typemasks=False, sequence_space=False)
-env.reset(seed=42)
-
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward', 'done'))
 
@@ -26,7 +20,7 @@ class ReplayBuffer(object):
         self.memory = deque([], capacity)
 
     def push(self, *args):
-        self.memory.append(*args)
+        self.memory.append(Transition(*args))
 
     def sample(self, batch_s):
         return random.sample(self.memory, batch_s)
@@ -40,28 +34,21 @@ class DQN(nn.Module):
     def __init__(self, n_dimensions, n_actions):
         super(DQN, self).__init__()
         flatten = n_dimensions[0] * n_dimensions[1]
-        self.layer1 = nn.Linear(flatten, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, n_actions)
+        self.layer1 = nn.Linear(flatten, 256)
+        self.layer2 = nn.Linear(256, 256)
+        self.layer3 = nn.Linear(256, n_actions)
 
     def forward(self, x):
         if isinstance(x, np.ndarray):
             x = torch.tensor(x, dtype=torch.float32)
-        x = x.view(x.shape[0], -1)
+        x = x.view(-1)
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         return self.layer3(x)
 
 
 # get the number of actions for each agent
-num_agent_act = []
-
-env.reset()  # Ensure the environment is reset before starting
-
-for agent in env.agents:  # Loop over each agent
-    num_agent_act.append(env.action_space(agent).n)  # Append the number of actions
-
-print(num_agent_act)
+num_agent_act = np.array([6, 6, 6, 6])
 
 
 class AgentDQN:
@@ -71,24 +58,25 @@ class AgentDQN:
         self.eps_start_val = eps_start_val
         self.eps_decay = eps_decay
         self.eps_end_val = eps_end_val
-        # self.tau = tau
         self.model = DQN(observation_size, num_actions)
         self.memory = ReplayBuffer(1000)
         self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
         self.steps_done = 0
 
-    def select_action(self, state):
-        agent_id = env.agent_selection
+    # if random returns random values between 0-5
+    def select_action(self, s):
+
         sample = random.random()
-        eps_threshold = self.eps_end_val + (self.eps_start_val - self.eps_end_val) * \
-                        math.exp(-1. * self.steps_done / self.eps_decay)
+        eps_threshold = self.eps_end_val + (self.eps_start_val - self.eps_end_val) * math.exp(
+            -1. * self.steps_done / self.eps_decay)
         self.steps_done += 1
         if sample > eps_threshold:
             with torch.no_grad():
-                print(self.model(state).max(1).indices.view(1, 1))
-                return self.model(state).max(1).indices.view(1, 1)
+                # return random.randrange(0, 6)
+
+                return self.model(s).max(0).indices.item()
         else:
-            return torch.tensor([[random.randrange(env.action_space(agent_id).n)]], dtype=torch.long)
+            return random.randrange(0, 6)
 
     def remember(self, s, a, r, new_s, d):
         self.memory.push(s, a, r, new_s, d)
@@ -97,14 +85,15 @@ class AgentDQN:
         if len(self.memory) < batch_size:
             return
         minibatch = self.memory.sample(batch_size)
-        for state, action, next_state, reward, done in minibatch:
+        for state, action, reward, next_state, done in minibatch:
             if not done:
                 target = reward + self.gamma * torch.max(
-                    self.model(torch.tensor(next_state, dtype=torch.float32))).item()
-                target_f = self.model(torch.tensor(state, dtype=torch.float32)).numpy()
+                    self.model(next_state))
+                target_f = self.model(state).detach().numpy()
                 target_f[action] = target
                 self.optimizer.zero_grad()
-                loss = nn.MSELoss()(torch.tensor(target_f), self.model(torch.tensor(state, dtype=torch.float32)))
+                # print(self.model(state))
+                loss = nn.MSELoss()(torch.tensor(target_f), self.model(state))
                 loss.backward()
                 self.optimizer.step()
             if self.eps_start_val > self.eps_end_val:
@@ -115,13 +104,7 @@ class AgentDQN:
 observation_shape = (27, 5)
 
 all_agents = ['archer_0', 'archer_1', 'knight_0', 'knight_1']
-# batch_size = 128
-# gamma = 0.99
-# eps_start_val = 0.9
-# eps_end_val = 0.05
-# eps_decay = 1000
-# tau = 0.005
-# learning_rate = 0.0001
+
 archer_1 = AgentDQN(learning_rate=0.0001, gamma=0.99, eps_start_val=0.9, eps_end_val=0.05, eps_decay=1000,
                     num_actions=num_agent_act[0], observation_size=observation_shape)
 
@@ -134,28 +117,61 @@ knight_1 = AgentDQN(learning_rate=0.0001, gamma=0.99, eps_start_val=0.9, eps_end
 knight_2 = AgentDQN(learning_rate=0.0001, gamma=0.99, eps_start_val=0.9, eps_end_val=0.05, eps_decay=1000,
                     num_actions=num_agent_act[3], observation_size=observation_shape)
 
-num_episodes = 1000
+env = knights_archers_zombies_v10.env(render_mode="human", spawn_rate=20, num_archers=2, num_knights=2, max_zombies=10,
+                                      max_arrows=10, killable_knights=True, killable_archers=True, pad_observation=True,
+                                      line_death=True, max_cycles=900,
+                                      vector_state=True, use_typemasks=False, sequence_space=False)
+
+num_episodes = 10
 agent_list = [archer_1, archer_2, knight_1, knight_2]
-batch_size = 50
+batch_size = 64
+last_observations = {agent: None for agent in agent_list}
+
+
+total_reward_vals = np.array([], dtype=int)
 for ep in range(num_episodes):
-    state = env.reset()
+    env.reset(seed=42)
     total_reward = 0
     done = False
-    for agent in agent_list:
-        observation, reward, termination, truncation, info = env.last()
-        if termination or truncation:
-            action = None
+    while not done:
+        for agent in env.agent_iter():
+            if agent == 'archer_0':
+                agent = agent_list[0]
+            elif agent == 'archer_1':
+                agent = agent_list[1]
+            elif agent == 'knight_0':
+                agent = agent_list[2]
+            else:
+                agent = agent_list[3]
 
-        else:
-            action = agent.select_action(observation)
-            next_state, reward, done, _ = env.step(action)
-            agent.remember(state, action, reward, next_state, done)
-            state = next_state
-            total_reward += reward
-            agent.replay(batch_size)
+            observation, reward, termination, truncation, info = env.last()
+            done = truncation or termination
+            if done:
+                action = None
 
-        print(f"Episode: {ep + 1}, Total Reward: {total_reward}")
+            else:
+                action = agent.select_action(observation)
 
+            env.step(action)
+
+            # initially the observation step is the current state but after the next iteration
+            # it becomes the next state for the replay buffer and the previous state becomes the current
+            if last_observations[agent] is not None:
+                agent.remember(last_observations[agent], action, reward, observation, termination)
+                total_reward += int(reward)
+                agent.replay(batch_size)
+
+            last_observations[agent] = observation
+
+        if done:
+            env.reset()
+            break
+
+    total_reward_vals = np.append(total_reward_vals, total_reward)
+    print(total_reward_vals)
+    print(f"Episode {ep + 1} finished with total reward: {total_reward}")
+
+env.close()
 # for agent in env.agent_iter():
 #     observation, reward, termination, truncation, info = env.last()
 #
